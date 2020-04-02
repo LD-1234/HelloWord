2dfd71e568ffd885fc7ec45050fda9ad6e4d4223
LiDeng <1172147422@qq.com>
2020-04-02 10:46:43 +0800

    第一次修改添加了日志打印

--- app/src/main/java/com/decard/helloword/MainActivity.java
+++ app/src/main/java/com/decard/helloword/MainActivity.java
@@ -3,6 +3,7 @@ package com.decard.helloword;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("Tag","这是第一次修改后添加的日志信息");
     }
 }
