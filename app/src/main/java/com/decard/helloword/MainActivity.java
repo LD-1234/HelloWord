60445d4bdaec156deb707d7c64f004357b43cb12
LiDeng <1172147422@qq.com>
2020-04-02 19:11:29 +0800

    添加回调方法。

--- app/src/main/java/com/decard/helloword/MainActivity.java
+++ app/src/main/java/com/decard/helloword/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import javax.security.auth.callback.Callback;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d("Tag","这是第一次修改后添加的日志信息");
+        new Thread(() -> new Response().handle("this is a messsage", data -> System.out.println("回调方法接收的数据："+data))).start();
+        System.out.println("异步回调,先做其他事");
     }
+}
+class Response{
+    public void handle(String msg, CallBack callback){
+        System.out.println("接收到的信息："+msg);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        callback.onResponse("请求完成，响应成功！");
+    }
+}
+interface CallBack{
+    void onResponse(String data);
 }
